de43142dc9ead4c4f6c33e4579f8c87d9f146962
BaibaBite <94292032+BaibaBite@users.noreply.github.com>
2023-02-06 15:00:50 +0200

    Create skripti.js

---
+++ skripti.js
@@ -0,0 +1,11 @@
+//alert("Čau!")
+console.log("tests");
+
+var logs = document.querySelector(".chataZinas");
+var vards = document.querySelector("#vards");
+var zina = document.querySelector("#zina");
+
+function sutitZinu(){
+    console.log("Poga darbojas");
+    console.log("ziņu ir: "+zina.value);
+}
