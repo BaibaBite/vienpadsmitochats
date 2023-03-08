4be3c07ca0dfde6d674d6f3ffb8d71a1891220bd
BaibaBite <94292032+BaibaBite@users.noreply.github.com>
2023-03-08 10:26:54 +0200

    Update skripti.js

--- skripti.js
+++ skripti.js
@@ -1,6 +1,7 @@
 //alert("Čau!")
 console.log("tests");
 
+const API = "https://suta-zinas-json-11.baibabite.repl.co"
 var logs = document.querySelector(".chataZinas");
 var vards = document.querySelector("#vards");
 var zina = document.querySelector("#zina");
@@ -10,10 +11,11 @@ function sutitZinu(){
     console.log("ziņu ir: "+zina.value);
 
     logs.innerHTML = logs.innerHTML +"</br>"+ zina.value;
+    fetch (API + '/sutit/' + vards.value + "/" + zina.value);
 }
 
 async function ieladetZinas(){
-    let datiNoServera = await fetch("zinas.txt");
+    let datiNoServera = await fetch(API +'/lasit');
     let dati = await datiNoServera.text();
     console.log(dati);
     logs.innerHTML = dati;
